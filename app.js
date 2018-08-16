ad510dd97aa9a1c6ef50e87bc2b59bd0528e7e99
Gabriel Lanata <gabriel.lanata@scotiabank.com.pe>
2018-08-16 14:37:25 -0500

    Updated sounds

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 var app = express(); // better instead
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.listen(1234);
 console.log("Listening on port 1234");
